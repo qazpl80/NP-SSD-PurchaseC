5bf2ab70405f7a7810d8c24afe0c6facc22ed8ee
Zhe Yu Chua <s10196605@connect.np.edu.sg>
2020-07-18 13:52:20 +0800

    Feedback form class

---
+++ Models/FeedbackForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurchaseC.Models
+{
+    public class FeedbackForm
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Computer Computer { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
