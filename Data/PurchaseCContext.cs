38f1847f9df3e28529b669953315fed0a821019b
Yuan Ming Tan <s10198319@connect.np.edu.sg>
2020-07-18 17:51:02 +0800

    version 2.4b (Add Condition for passwords during registration)

--- Data/PurchaseCContext.cs
+++ Data/PurchaseCContext.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using PurchaseC.Models;
+﻿using PurchaseC.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 
 namespace PurchaseC.Data
 {
--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,34 @@ namespace PurchaseC
                     .AddEntityFrameworkStores<PurchaseCContext>()
                     .AddDefaultTokenProviders();
 
+            services.AddMvc()
+            .AddRazorPagesOptions(options =>
+            {
+                // options.Conventions.AllowAnonymousToFolder("/Computers");
+                // options.Conventions.AuthorizePage("/Computers/Create");
+                //  options.Conventions.AuthorizeAreaPage("Identity", "/Manage/Accounts");
+                options.Conventions.AuthorizeFolder("/Computers");
+            });
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
