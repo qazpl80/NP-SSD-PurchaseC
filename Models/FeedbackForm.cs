877e78a4cef5c1cc5a2d7526bb3f15aae204bd2b
Zhe Yu Chua <s10196605@connect.np.edu.sg>
2020-08-03 16:37:00 +0800

    Some input validation for feedback form

--- Models/FeedbackForm.cs
+++ Models/FeedbackForm.cs
@@ -9,12 +9,20 @@ namespace PurchaseC.Models
     public class FeedbackForm
     {
         public int ID { get; set; }
+
         [Required]
+        [RegularExpression(@"^[a-zA-Z0-9""'\s-]*$", ErrorMessage = "Entered Text contain Invalid characters")]
+        [Display(Name = "Name/Username")]
         public string Name { get; set; }
+
         [Required]
+        [RegularExpression(@"^[a-zA-Z""]*$", ErrorMessage = "Entered Text contain Invalid characters")]
         public string Computer { get; set; }
+
         [Required]
         public int Rating { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9""'\s-]*$", ErrorMessage = "Entered Text contain Invalid characters")]
         [StringLength(500)]
         public string Comment { get; set; }
     }
