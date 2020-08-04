a08608420d4fcabf6b206f3a792db4888b624330
qazpl80 <s10198319@connect.np.edu.sg>
2020-08-04 16:36:32 +0800

    Naming for User, Role & Computer

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,15 +1,26 @@
 ﻿using System;
+using System.ComponentModel;
 using Microsoft.AspNetCore.Identity;
 namespace PurchaseC.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [DisplayName("Full Name")]
         public string FullName { get; set; }
+
+        [DisplayName("Birth Date")]
         public DateTime BirthDate { get; set; }
+
+        [DisplayName("Contact Number")]
         public int ContactNo { get; set; }
+
+        [DisplayName("Home Address")]
         public string HomeAddress { get; set; }
+
         public string Country { get; set; }
+
         public string Citizenship { get; set; }
+
         public string Gender { get; set; }
     }
 }
--- Models/Computer.cs
+++ Models/Computer.cs
@@ -4,26 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
-
+using System.ComponentModel;
 
 namespace PurchaseC.Models
 {
     public class Computer
     {
         public int ID { get; set; }
+
+        [DisplayName("Name")]
         public string Name { get; set; }
 
         [DataType(DataType.Date)]
+        [DisplayName("Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [DisplayName("Memory")]
         public string Memory { get; set; }
+
+        [DisplayName("Processor")]
         public string Processor { get; set; }
+
+        [DisplayName("GPU")]
         public string GPU { get; set; }
+
+        [DisplayName("Storage Info")]
         public string Storage_Info { get; set; }
+
+        [DisplayName("Operating System")]
         public string OS { get; set; }
+
+        [DisplayName("PSU")]
         public string PSU { get; set; }
+
+        [DisplayName("Motherboard")]
         public string Motherboard { get; set; }
+
         [Column(TypeName = "decimal(18, 2)")]
+        [DisplayName("Price")]
         public decimal Price { get; set; }
     }
 }
